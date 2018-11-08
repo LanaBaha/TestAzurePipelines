4792d2e5b38ea0aabdbc8e456a15941d67759237
Lana Azad Baha <Lab@kmd.dk>
2018-11-08 14:24:29 +0100

    First commit

---
+++ Mycoreapp2/Data/Mycoreapp2Context.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mycoreapp2.Models
+{
+    public class Mycoreapp2Context : DbContext
+    {
+        public Mycoreapp2Context (DbContextOptions<Mycoreapp2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Mycoreapp2.Models.Movie> Movie { get; set; }
+    }
+}
---
+++ Mycoreapp2/Migrations/20181107141947_Ratings2.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Mycoreapp2.Migrations
+{
+    public partial class Ratings2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
